3fc9d64fd32d66cfac59243a39cdeb79668733ba
Vibodh Bhosure <vibodhbhoure@gmail.com>
2023-10-20 21:25:54 +0530

    Initial

---
+++ client/src/components/Display.js
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import "./Display.css";
+const Display = ({ contract, account }) => {
+  const [data, setData] = useState("");
+  const getdata = async () => {
+    let dataArray;
+    const Otheraddress = document.querySelector(".address").value;
+    try {
+      if (Otheraddress) {
+        dataArray = await contract.display(Otheraddress);
+        console.log(dataArray);
+      } else {
+        dataArray = await contract.display(account);
+      }
+    } catch (e) {
+      alert("You don't have access");
+    }
+    const isEmpty = Object.keys(dataArray).length === 0;
+
+    if (!isEmpty) {
+      const str = dataArray.toString();
+      const str_array = str.split(",");
+      // console.log(str);
+      // console.log(str_array);
+      const images = str_array.map((item, i) => {
+        console.log(item.substring(34));
+        return (
+          <table>
+            <tbody>
+              <a href={item} key={i} target="_blank">
+                <p
+                  key={i}
+                  src={`https://gateway.pinata.cloud/ipfs/${item.substring(34)}`}
+                  alt="new"
+                  className="image-list"
+                >{item.substring(34)}</p>
+              </a>
+            </tbody>
+          </table>
+        );
+      });
+      setData(images);
+    } else {
+      alert("No image to display");
+    }
+  };
+  return (
+    <>
+      <div className="image-list">{data}</div>
+      <input
+        type="text"
+        placeholder="Enter Address"
+        className="address"
+      ></input>
+      <button className="center button" onClick={getdata}>
+        Get Data
+      </button>
+    </>
+  );
+};
+export default Display;
